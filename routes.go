a764cf578bc64e8caffa341d6722f717a4253a29
Tom Szymanski <szymanski.tom@gmail.com>
2015-05-07 00:00:04 -0600

    Add FetchAllStatuses or else you never have a status

--- routes.go
+++ routes.go
@@ -119,7 +119,7 @@ func SetGames(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Write(js)
 }
 
-func SetStatus(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func SetStatuses(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	c := appengine.NewContext(r)
 	// If before 12pm UTC (8am EST). Display the results from the day before
 	gameTime := time.Now().UTC()
@@ -243,12 +243,115 @@ func SetStatus(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Write(js)
 }
 
+func SetAllStatuses(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	c := appengine.NewContext(r)
+	// If before 12pm UTC (8am EST). Display the results from the day before
+	gameTime := time.Now().UTC()
+	if gameTime.Hour() < 12 {
+		gameTime = time.Now().UTC().Add(-12 * time.Hour)
+	}
+
+	// Get all statuses
+	q := datastore.NewQuery("Status")
+	ls := []status{}
+	_, Err := q.GetAll(c, &ls)
+	if Err != nil {
+		http.Error(w, Err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	statuses := []status{}
+	msb := MasterScoreboard(gameTime, r)
+	for _, g := range msb.Data.Games.Game {
+		outs, _ := strconv.Atoi(g.GameStatus.Outs)
+		base_runners, _ := strconv.Atoi(g.RunnersOnBase.Status)
+
+		home_team_runs, _ := strconv.Atoi(g.LineScore.Runs.Home)
+		away_team_runs, _ := strconv.Atoi(g.LineScore.Runs.Away)
+		run_diff := home_team_runs - away_team_runs
+
+		inning, _ := strconv.Atoi(g.GameStatus.Inning)
+		top := false
+		if g.GameStatus.TopInning == "Y" {
+			top = true
+		}
+
+		if run_diff > 4 {
+			run_diff = 4
+		}
+		if run_diff < -4 {
+			run_diff = -4
+		}
+
+		if outs > 2 && top == true {
+			outs = 0
+			top = false
+		}
+		if outs > 2 && top == false {
+			outs = 0
+			top = true
+			inning++
+		}
+		if inning > 9 {
+			inning = 9
+		}
+
+		bo := (outs + 1) * base_runners
+		gs := GameState(inning, top, run_diff)
+		li := 0.0
+		if g.GameStatus.Status == "In Progress" {
+			li = LeverageIndex(bo, gs)
+		}
+
+		//convert from mlbApiGame to status
+		s := status{}
+		s.GameId, _ = strconv.Atoi(g.GamePk)
+		s.State = g.GameStatus.Status
+		s.Score.Home = home_team_runs
+		s.Score.Away = away_team_runs
+		s.BaseRunnerState = base_runners
+		s.Inning, _ = strconv.Atoi(g.GameStatus.Inning)
+		s.HalfInning = "Bottom"
+		if g.GameStatus.TopInning == "Y" {
+			s.HalfInning = "Top"
+		}
+		s.Count.Balls, _ = strconv.Atoi(g.GameStatus.Balls)
+		s.Count.Strikes, _ = strconv.Atoi(g.GameStatus.Strikes)
+		s.Outs, _ = strconv.Atoi(g.GameStatus.Outs)
+		s.Li = li
+
+		statuses = append(statuses, s)
+	}
+
+	// store statuses
+	keys := make([]*datastore.Key, len(statuses))
+	for k := range keys {
+		keys[k] = datastore.NewKey(c, "Status", "", int64(statuses[k].GameId), nil)
+	}
+
+	_, err := datastore.PutMulti(c, keys, statuses)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	js, err := json.Marshal(statuses)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(js)
+}
+
 func Routes() http.Handler {
 	router := httprouter.New()
 
 	router.GET("/games", GameJSON)
 	router.GET("/fetchGames", SetGames)
-	router.GET("/fetchStatuses", SetStatus)
+	router.GET("/fetchStatuses", SetStatuses)
+	router.GET("/fetchAllStatuses", SetAllStatuses)
 	router.NotFound = http.FileServer(http.Dir("static/")).ServeHTTP
 
 	return router
