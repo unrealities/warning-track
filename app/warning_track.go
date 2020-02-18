c230a0b6e3298639696b9d3a3d2770250baccc6a
Tom Szymanski <szymanski.tom@gmail.com>
2020-02-18 13:35:45 -0700

    3-0-3 Page renders. Error when fetching games (may have been pre-existing)

--- app/warning_track.go
+++ app/warning_track.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/unrealities/warning-track/routers"
 )
@@ -11,4 +13,17 @@ func main() {
 	http.Handle("/", r)
 	http.HandleFunc("/tv", routers.RedirectHandler("/tv.html"))
 	http.HandleFunc("/faq", routers.RedirectHandler("/faq.html"))
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
 }
