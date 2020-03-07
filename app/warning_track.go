a572d665b9d16e018e1882a919d0cfa7854b193c
Tom Szymanski <szymanski.tom@gmail.com>
2020-03-07 05:45:47 -0700

    4-0-6 remove ListenAdnServe

--- app/warning_track.go
+++ app/warning_track.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/unrealities/warning-track/routers"
 	"google.golang.org/appengine"
 )
 
-func init() {
+func main() {
 	r := routers.Routes()
 	http.Handle("/", r)
 	http.HandleFunc("/tv", routers.RedirectHandler("/tv.html"))
 	http.HandleFunc("/faq", routers.RedirectHandler("/faq.html"))
-
-	// [START setting_port]
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, r); err != nil {
-		log.Fatal(err)
-	}
-	// [END setting_port]
 
 	appengine.Main()
 }
